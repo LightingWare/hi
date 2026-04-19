1668ecbf54f8198403114a1d05adf112156fbec6
LightningWare <146706767+LightingWare@users.noreply.github.com>
2026-04-20 09:37:59 +1000

    Update Silly Hub.lua

--- Silly Hub.lua
+++ Silly Hub.lua
@@ -45,5 +45,14 @@ local Button = Tab:CreateButton({
          
    end,
 })
+local Toggle = Tab:CreateToggle({
+   Name = "Nothing",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file; make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   -- The function that takes place when the toggle is pressed
+   -- The variable (Value) is a boolean on whether the toggle is true or false
+   end,
+})
 
 Rayfield:LoadConfiguration()
